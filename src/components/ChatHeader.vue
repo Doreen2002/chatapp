34d3b47f234d909bf996cdf8dbf434ada19b5e44
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-12-28 11:01:30 +0200

    added heroicons, tailwind, styles header for chat section

--- src/components/ChatHeader.vue
+++ src/components/ChatHeader.vue
@@ -1,11 +1,18 @@
 <script setup>
-
+import { Bars3Icon,PlusIcon , MagnifyingGlassIcon} from '@heroicons/vue/24/solid'
 </script>
 
 
 <template>
-    <div>
-        <div class="bg-sky-300"></div>
-        <div></div>
+    <div class="max-w-3xl">
+        <div class="flex flex-col ">
+            <div class="flex justify-between h-10 bg-pink-300 place-items-center px-3 text-white">
+                <div><Bars3Icon class="size-6 " /></div>
+                <div >Messages </div>
+                <div><PlusIcon class="size-6 " /></div>
+            </div>
+            <div class="flex place-self-center place-items-center my-2 border rounded-md w-96 text-gray-400"><input type="search" placeholder="Search" class="flex-grow  text-center  h-7"/> <MagnifyingGlassIcon class="size-6 "/></div>
+        </div>
+       
     </div>
 </template>
