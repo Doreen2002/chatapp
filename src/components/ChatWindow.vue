4158c31ba0f2cce02d479ed37eb4da8593a3d2c4
Doreen2002 <doreenmwapekatebe8@gmail.com>
2024-12-30 11:33:16 +0200

    initial design for chat window

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -2,8 +2,28 @@
 </script>
 
 <template>
-    <div>
-        <div class="senders"></div>
-        <div class="receivers"></div>
+    <div class="px-3 absolute bg-gray-100 w-full ">
+       
+    <div >
+        <div class="flex w-full  justify-start">
+            <div class="senders w-48 h-6 border border-pink-200 rounded-br-lg"></div>
+        </div>
+        
+        <div class="flex w-full justify-end py-5">
+            <div class="receivers w-48 h-6 border border-pink-200  rounded-bl-lg place-self-end"></div>
+        </div>
+       
     </div>
+    <div >
+        <div class="flex w-full  justify-start">
+            <div class="senders w-48 h-6 border border-pink-200 rounded-br-lg"></div>
+        </div>
+        
+        <div class="flex w-full justify-end py-5">
+            <div class="receivers w-48 h-6 border border-pink-200  rounded-bl-lg place-self-end"></div>
+        </div>
+       
+    </div>
+    </div>
+    
 </template>
--- src/components/MessageList.vue
+++ src/components/MessageList.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import ChatWindow from './ChatWindow.vue';
 
 const messages = ref([
     {
@@ -27,19 +28,28 @@ const messages = ref([
 
     }
 ])
+const properties = ref({
+    position:{x:0, y:0} 
+})
+
+const showWindow_func = ()=>
+{
+   properties.value.position.x = 100;
+   properties.value.position.y = 100;
+}
 </script>
 
 
 
 <template>
    
-    <div class="flex mx-3 my-2 border rounded-t-lg border-pink-100 p-4" v-for="(item, key) in messages" :key="key">
+    <div @click="showWindow_func()" class="flex mx-3 my-2 border rounded-t-lg border-pink-100 p-4" v-for="(item, key) in messages" :key="key">
         <div><img :src=item.image class="w-16 h-16 border rounded-full"/></div>
         <div class="flex flex-col flex-grow  ">
             <div class="flex justify-between"><div>{{ item.username }}</div><div>{{ item.dateTime }}</div></div>
             <div class="flex justify-between"><div>{{ item.message }}</div><div>{{ item.unread }}</div></div>
         </div>
     </div>
-    
+    <ChatWindow :style="{ top: `${properties.position.x}px`, left: `${properties.position.y}px` }"/>
     
 </template>
